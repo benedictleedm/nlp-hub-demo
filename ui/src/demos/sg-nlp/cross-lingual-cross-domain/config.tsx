d09f8eebb3e61a88d6d7770b6a774ef906829d57
raymondng76 <raymond.bcng@gmail.com>
2021-08-03 14:11:59 +0800

    [#10] update NEA and UFD description based on feedback

--- ui/src/demos/sg-nlp/cross-lingual-cross-domain/config.tsx
+++ ui/src/demos/sg-nlp/cross-lingual-cross-domain/config.tsx
@@ -5,7 +5,11 @@ import { ModelConfig } from "../../models";
 const models: ModelConfig[] = [
   {
     id: "ufd",
-    desc: "This model is based on the Unsupervised Feature Decomposition method.",
+    desc: "This model is based on the Unsupervised Feature Decomposition method.\
+           English source language is used for training the UFD model for this demo and feature models best\
+           evaluated for each target languages and target domains combination. Input text must match the target\
+           langauge and target domain selected in the dropdown list. Running the demo will perform inference for\
+           models evaluated best for each cross-domains of the selected target domain.",
     displayName: "Unsupervised Feature Decomposition (UFD) Model",
     usage: {
       prediction: `from sgnlp.models.ufd import UFDModelBuilder, UFDPreprocessor
@@ -21,9 +25,9 @@ preprocessor = UFDPreprocessor()
 model_groups = model_builder.build_model_group()
 
 # Model predict ('books_de_dvd' model example)
-instance = """Wolverine is BACK Der Film ist im Grunde wie alle Teile der X-Men für Comic-Fans auf jeden Fall ein muss.
-              Hugh Jackman spielt seine Rolle wie immer so gut was ich von den ein oder anderen Darsteller leider nicht 
-              sagen kann. Story und Action sind aber genug Gründe um sich die Blu-ray zu kaufen."""
+instance = "Wolverine is BACK Der Film ist im Grunde wie alle Teile der X-Men für Comic-Fans auf jeden Fall ein muss.
+            Hugh Jackman spielt seine Rolle wie immer so gut was ich von den ein oder anderen Darsteller leider nicht
+            sagen kann. Story und Action sind aber genug Gründe um sich die Blu-ray zu kaufen."
 
 instance_features = preprocessor([instance])
 output = model_groups['books_de_dvd'](**instance_features)`,
