4473e6e5a6414f682ad247a3121c180fe7af65ef
Benedict <benedictlee@aisingapore.org>
2021-08-03 20:31:19 +0800

    [#10] Expand descriptions for essay scoring and ufd

--- ui/src/demos/sg-nlp/cross-lingual-cross-domain/config.tsx
+++ ui/src/demos/sg-nlp/cross-lingual-cross-domain/config.tsx
@@ -5,11 +5,12 @@ import { ModelConfig } from "../../models";
 const models: ModelConfig[] = [
   {
     id: "ufd",
-    desc: "This model is based on the Unsupervised Feature Decomposition method.\
-           English source language is used for training the UFD model for this demo and feature models best\
-           evaluated for each target languages and target domains combination. Input text must match the target\
-           langauge and target domain selected in the dropdown list. Running the demo will perform inference for\
-           models evaluated best for each cross-domains of the selected target domain.",
+    desc: "This model is based on the Unsupervised Feature Decomposition method introduced in the accompanying paper.\
+           The source language is always English while the source domain could be either Books, DVDs or Music reviews.\
+           The source domain will be automatically selected in the backend and will be different from the selected\
+           target domain. The target language and target domain selected in the dropdown lists must match the input\
+           text's language and domain. This demo showcases the best performing models for each target language and\
+           target domain.",
     displayName: "Unsupervised Feature Decomposition (UFD) Model",
     usage: {
       prediction: `from sgnlp.models.ufd import UFDModelBuilder, UFDPreprocessor
@@ -38,8 +39,12 @@ output = model_groups['books_de_dvd'](**instance_features)`,
 const config: DemoConfig = {
   taskId: "cross-lingual-cross-domain",
   title: "Cross Lingual Cross Domain",
-  desc: `Cross-lingual cross-domain is the task of predicting on a target language and a target domain 
-  when there is no labelled data of the target domain in the source and target language.`,
+  desc: `Cross-lingual cross-domain sentiment analysis is the task of using a model trained on one language and one domain
+  to predict the sentiment of input texts that belong to a different language and a different domain. The language and domain 
+  that the model was trained on is referred to as the source language and source domain. The language and domain of the
+  input texts is the target language and target domain. These models are extremely useful in the low resource setting where
+  there is little to no available labelled data for the target language and target domain but considerably more data for the
+  source language and source domain.`,
   models: models,
 };
 
