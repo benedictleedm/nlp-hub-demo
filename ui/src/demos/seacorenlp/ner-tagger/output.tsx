0aabdc5334ac8bcd00211a36d96afcbf4cd4d13e
Wei Qi <wqleong24@gmail.com>
2021-08-13 11:14:31 +0800

    Refactor NER Tagset

--- ui/src/demos/seacorenlp/ner-tagger/output.tsx
+++ ui/src/demos/seacorenlp/ner-tagger/output.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Highlight } from "../../../components/output/highlight/Highlight";
 import { HighlightContainer } from "../../../components/output/highlight/HighlightContainer";
+import entityLookup from "./tagsets";
 
 interface OutputProps {
   responseData: {
@@ -15,108 +16,6 @@ interface Token {
 }
 
 const TokenSpan = ({ token }: any) => {
-  const entityLookup: any = {
-    "PER": {
-      tooltip: "Person",
-      color: "pink"
-    },
-    "LOC": {
-      tooltip: "Location",
-      color: "green"
-    },
-    "ORG": {
-      tooltip: "Organization",
-      color: "blue"
-    },
-    "MISC": {
-      tooltip: "Miscellaneous",
-      color: "gray"
-    },
-    "CARDINAL": {
-      tooltip: "Cardinal Number",
-      color: "orange"
-    },
-    "EVENT": {
-      tooltip: "Event",
-      color: "green"
-    },
-    "DATE": {
-      tooltip: "Date",
-      color: "fuchsia"
-    },
-    "FAC": {
-      tooltip: "Facility",
-      color: "cobalt"
-    },
-    "GPE": {
-      tooltip: "Country/City/State",
-      color: "teal"
-    },
-    "LANGUAGE": {
-      tooltip: "Language",
-      color: "red"
-    },
-    "LAW": {
-      tooltip: "Law",
-      color: "brown"
-    },
-    // LOC - see above
-    "MONEY": {
-      tooltip: "Monetary Value",
-      color: "orange"
-    },
-    "NORP": {
-      tooltip: "Nationalities, Religious/Political Groups",
-      color: "green"
-    },
-    "ORDINAL": {
-      tooltip: "Ordinal Value",
-      color: "orange"
-    },
-    // ORG - see above.
-    "PERCENT": {
-      tooltip: "Percentage",
-      color: "orange"
-    },
-    "PRODUCT": {
-      tooltip: "Product",
-      color: "purple"
-    },
-    "QUANTITY": {
-      tooltip: "Quantity",
-      color: "orange"
-    },
-    "TIME": {
-      tooltip: "Time",
-      color: "fuchsia"
-    },
-    "WORK_OF_ART": {
-      tooltip: "Work of Art/Media",
-      color: "tan"
-    },
-    // PyThaiNLP Entities
-    "ZIP": {
-      tooltip: "Zip Code",
-      color: "green"
-    },
-    "LEN": {
-      tooltip: "Length",
-      color: "orange"
-    },
-    "URL": {
-      tooltip: "URL",
-      color: "black"
-    },
-    "EMAIL": {
-      tooltip: "Email",
-      color: "black"
-    },
-    "PHONE": {
-      tooltip: "Phone Number",
-      color: "orange"
-    },
-  }
-
   const entity = token.entity;
 
   if (entity !== null) {
