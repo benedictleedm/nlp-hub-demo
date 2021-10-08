9f26be40c9c317a9ed49f6cd84e1e270e2f8196b
Jonathan Heng <jonheng@users.noreply.github.com>
2021-10-08 15:45:34 +0800

    Minor formatting change

--- ui/src/demos/sg-nlp/cross-sentence-gec/examples.tsx
+++ ui/src/demos/sg-nlp/cross-sentence-gec/examples.tsx
@@ -1,22 +1,16 @@
 const examples: Record<string, any>[] = [
-    {
-      display:
-        "All of us are living in the technology realm society..",
-      text:
-      "All of us are living in the technology realm society. Have you ever wondered why we use these tools to connect ourselves with other people? It started withthe invention of technology which has evolved tremendously over the past few decades. In the past, we travel by ship and now we can use airplane to do so. In the past, it took a few days to receive a message as we need to post our letter and now, we can use e-mail which stands for electronic message to send messages to our friends or even use our handphone to send our messages.",
-    },
-    {
-      display:
-        "Machines have replaced a bunch of coolies and heavy labor..",
-      text:
-        "Machines have replaced a bunch of coolies and heavy labor. Cars and trucks diminish the redundancy of long time shipment. As a result, people have more time to enjoy advantage of modern life. One can easily travel to the other half of the globe to see beautiful scenery that one dreams for his lifetime. One can also easily see his deeply loved one through internet from miles away.",
-    },
-    {
-      display:
-        "A dictionary (Dict) can also is called a map in JavaScript..",
-      text:
-      "A dictionary (Dict) can also is called a map in JavaScript, and maps/dictionaries is used to store unique element of key-value pairs.",
-    },
-  ];
+  {
+    display: "All of us are living in the technology realm society..",
+    text: "All of us are living in the technology realm society. Have you ever wondered why we use these tools to connect ourselves with other people? It started withthe invention of technology which has evolved tremendously over the past few decades. In the past, we travel by ship and now we can use airplane to do so. In the past, it took a few days to receive a message as we need to post our letter and now, we can use e-mail which stands for electronic message to send messages to our friends or even use our handphone to send our messages.",
+  },
+  {
+    display: "Machines have replaced a bunch of coolies and heavy labor..",
+    text: "Machines have replaced a bunch of coolies and heavy labor. Cars and trucks diminish the redundancy of long time shipment. As a result, people have more time to enjoy advantage of modern life. One can easily travel to the other half of the globe to see beautiful scenery that one dreams for his lifetime. One can also easily see his deeply loved one through internet from miles away.",
+  },
+  {
+    display: "A dictionary (Dict) can also is called a map in JavaScript..",
+    text: "A dictionary (Dict) can also is called a map in JavaScript, and maps/dictionaries is used to store unique element of key-value pairs.",
+  },
+];
 
-  export default examples;
+export default examples;
