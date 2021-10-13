9ffe406fe17ba3d74eb32671058882b9263d07a9
Jonathan Heng <jonheng@users.noreply.github.com>
2021-10-13 14:18:24 +0800

    Update csgec demo to show model usage from backend

--- ui/src/demos/sg-nlp/cross-sentence-gec/config.tsx
+++ ui/src/demos/sg-nlp/cross-sentence-gec/config.tsx
@@ -4,10 +4,14 @@ import { ModelConfig } from "../../models";
 const models: ModelConfig[] = [
   {
     id: "csgec",
-    desc: "This model is based on the convolutional encoder-decoder architecture and also " +
+    desc:
+      "This model is based on the convolutional encoder-decoder architecture and also " +
       "incorporates cross-sentence context. The two preceding sentences are used as the " +
       "cross-sentence context. If there is only one sentence, no context will be included.",
     displayName: "Cross Sentence GEC",
+    usage: {
+      fromBackend: true,
+    },
   },
 ];
 
@@ -18,7 +22,7 @@ const config: DemoConfig = {
     "Grammatical Error Correction is the task of correcting spelling, punctuation, " +
     " grammatical, and word choice errors in text. The input is a potentially erroneous " +
     "sentence and the system is expected to output a corrected sentence. For this demo, you " +
-    "may specify more than one sentence in the \"Original Sentences\" section and the demo " +
+    'may specify more than one sentence in the "Original Sentences" section and the demo ' +
     "will automatically split the paragraph into sentences.",
   models: models,
 };
